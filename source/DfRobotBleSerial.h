4ecd682c31372f1931be9ecd7d17f0ca5de9dfb6
Matthew McKee <matt_mckee@outlook.com>
2016-04-25 16:08:25 -0700

    change DfRobotBleSerial to use base BleSerial implementation

--- source/DfRobotBleSerial.h
+++ source/DfRobotBleSerial.h
@@ -1,236 +1,279 @@
 /*
-    Copyright(c) Microsoft Open Technologies, Inc. All rights reserved.
+Copyright(c) Microsoft Open Technologies, Inc. All rights reserved.
 
-    The MIT License(MIT)
+The MIT License(MIT)
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files(the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions :
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
 
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 
 #pragma once
 #include "IStream.h"
+#include "BleSerial.h"
 #include <mutex>
 #include <queue>
 
 namespace Microsoft {
-namespace Maker {
-namespace Serial {
+    namespace Maker {
+        namespace Serial {
 
-public ref class DfRobotBleSerial sealed : public IStream
-{
-public:
-    virtual event IStreamConnectionCallback ^ConnectionEstablished;
-    virtual event IStreamConnectionCallbackWithMessage ^ConnectionLost;
-    virtual event IStreamConnectionCallbackWithMessage ^ConnectionFailed;
+            public ref class DfRobotBleSerial sealed : public IStream
+            {
+            public:
+                virtual event IStreamConnectionCallback ^ConnectionEstablished;
+                virtual event IStreamConnectionCallbackWithMessage ^ConnectionLost;
+                virtual event IStreamConnectionCallbackWithMessage ^ConnectionFailed;
 
-    ///<summary>
-    ///A constructor which accepts a string corresponding to a device name or ID to connect to.
-    ///</summary>
-    DfRobotBleSerial(
-        Platform::String ^device_name_
-        );
+                ///<summary>
+                ///A constructor which accepts a string corresponding to a device name or ID to connect to.
+                ///</summary>
+                DfRobotBleSerial(
+                    Platform::String ^device_name_
+                )
+                {
+                    _bleSerial = ref new BleSerial(
+                        device_name_,
+                        Platform::Guid{ 0x6E400001, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) },
+                        Platform::Guid{ 0x6E400002, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) },
+                        Platform::Guid{ 0x6E400003, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) }
+                    );
+                };
 
-    ///<summary>
-    ///A constructor which accepts a DeviceInformation object to explicitly specify which device to connect to.
-    ///</summary>
-    [Windows::Foundation::Metadata::DefaultOverloadAttribute]
-    DfRobotBleSerial(
-        Windows::Devices::Enumeration::DeviceInformation ^device_
-        );
+                ///<summary>
+                ///A constructor which accepts a DeviceInformation object to explicitly specify which device to connect to.
+                ///</summary>
+                [Windows::Foundation::Metadata::DefaultOverloadAttribute]
+                DfRobotBleSerial(
+                    Windows::Devices::Enumeration::DeviceInformation ^device_
+                )
+                {
+                    _bleSerial = ref new BleSerial(
+                        device_,
+                        Platform::Guid{ 0x6E400001, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) },
+                        Platform::Guid{ 0x6E400002, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) },
+                        Platform::Guid{ 0x6E400003, 0xB5A3, 0xF393, (0xE0, 0xA9, 0xE5, 0x0E, 0x24, 0xDC, 0xCA, 0x9E) }
+                    );
+                };
 
-    virtual
-    ~DfRobotBleSerial(
-        void
-        );
+                virtual
+                    ~DfRobotBleSerial(
+                        void
+                    )
+                {
+                    _bleSerial->~BleSerial();
+                };
 
-    virtual
-    uint16_t
-    available(
-        void
-        );
+                virtual
+                    uint16_t
+                    available(
+                        void
+                    )
+                {
+                    return _bleSerial->available();
+                };
 
-    [Windows::Foundation::Metadata::DefaultOverloadAttribute]
-    inline
-    void
-    begin(
-        void
-        )
-    {
-        //baud rate and serial configuration are unnecessary for BLE connections as we are not using asyncronous TTL
-        begin( NULL, SerialConfig::NONE );
-    }
+                [Windows::Foundation::Metadata::DefaultOverloadAttribute]
+                inline
+                    void
+                    begin(
+                        void
+                    )
+                {
+                    //baud rate and serial configuration are unnecessary for BLE connections as we are not using asyncronous TTL
+                    _bleSerial->begin();
+                }
 
-    virtual
-    void
-    begin(
-        uint32_t baud_,
-        SerialConfig config_
-        );
+                virtual
+                    void
+                    begin(
+                        uint32_t baud_,
+                        SerialConfig config_
+                    )
+                {
+                    _bleSerial->begin(baud_, config_);
+                };
 
-    virtual
-    bool
-    connectionReady(
-        void
-        );
+                virtual
+                    bool
+                    connectionReady(
+                        void
+                    )
+                {
+                    _bleSerial->connectionReady();
+                };
 
-    virtual
-    void
-    end(
-        void
-        );
+                virtual
+                    void
+                    end(
+                        void
+                    )
+                {
+                    _bleSerial->end();
+                };
 
-    virtual
-    void
-    flush(
-        void
-        );
+                virtual
+                    void
+                    flush(
+                        void
+                    )
+                {
+                    _bleSerial->flush();
+                };
 
-    virtual
-    void
-    lock(
-        void
-        );
+                virtual
+                    void
+                    lock(
+                        void
+                    )
+                {
+                    _bleSerial->lock();
+                };
 
-    virtual
-    uint16_t
-    print(
-          uint8_t c_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        uint8_t c_
+                    )
+                {
+                    return _bleSerial->print(c_);
+                };
 
-    virtual
-    uint16_t
-    print(
-          int32_t value_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        int32_t value_
+                    )
+                {
+                    return _bleSerial->print(value_);
+                };
 
-    virtual
-    uint16_t
-    print(
-          int32_t value_,
-          Radix base_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        int32_t value_,
+                        Radix base_
+                    )
+                {
+                    return _bleSerial->print(value_, base_);
+                };
 
-    virtual
-    uint16_t
-    print(
-          uint32_t value_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        uint32_t value_
+                    )
+                {
+                    return _bleSerial->print(value_);
+                };
 
-    virtual
-    uint16_t
-    print(
-          uint32_t value_,
-          Radix base_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        uint32_t value_,
+                        Radix base_
+                    )
+                {
+                    return _bleSerial->print(value_, base_);
+                };
 
-    virtual
-    uint16_t
-    print(
-          double value_
-         );
+                virtual
+                    uint16_t
+                    print(
+                        double value_
+                    )
+                {
+                    return _bleSerial->print(value_);
+                };
 
-    [Windows::Foundation::Metadata::DefaultOverloadAttribute]
-    virtual
-    uint16_t
-    print(
-          double value_,
-          int16_t decimal_place_
-         );
+                [Windows::Foundation::Metadata::DefaultOverloadAttribute]
+                virtual
+                    uint16_t
+                    print(
+                        double value_,
+                        int16_t decimal_place_
+                    )
+                {
+                    return _bleSerial->print(value_, decimal_place_);
+                };
 
-    [Windows::Foundation::Metadata::DefaultOverloadAttribute]
-    virtual
-    uint16_t
-    print(
-        const Platform::Array<uint8_t> ^buffer_
-        );
+                [Windows::Foundation::Metadata::DefaultOverloadAttribute]
+                virtual
+                    uint16_t
+                    print(
+                        const Platform::Array<uint8_t> ^buffer_
+                    )
+                {
+                    return _bleSerial->print(buffer_);
+                };
 
-    virtual
-    uint16_t
-    read(
-        void
-        );
+                virtual
+                    uint16_t
+                    read(
+                        void
+                    )
+                {
+                    return _bleSerial->read();
+                };
 
-    virtual
-    void
-    unlock(
-        void
-        );
+                virtual
+                    void
+                    unlock(
+                        void
+                    )
+                {
+                    _bleSerial->unlock();
+                };
 
-    virtual
-    uint16_t
-    write(
-        uint8_t c_
-        );
+                virtual
+                    uint16_t
+                    write(
+                        uint8_t c_
+                    )
+                {
+                    return _bleSerial->write(c_);
+                };
 
-    [Windows::Foundation::Metadata::DefaultOverloadAttribute]
-    virtual
-    uint16_t
-    write(
-        const Platform::Array<uint8_t> ^buffer_
-        );
+                [Windows::Foundation::Metadata::DefaultOverloadAttribute]
+                virtual
+                    uint16_t
+                    write(
+                        const Platform::Array<uint8_t> ^buffer_
+                    )
+                {
+                    return _bleSerial->write(buffer_);
+                };
 
-    ///<summary>
-    ///Begins an asyncronous request for all Bluetooth LE devices that are paired and may be used to attempt a device connection.
-    ///</summary>
-    static
-    Windows::Foundation::IAsyncOperation<Windows::Devices::Enumeration::DeviceInformationCollection ^> ^
-    listAvailableDevicesAsync(
-        void
-        );
+                ///<summary>
+                ///Begins an asyncronous request for all Bluetooth LE devices that are paired and may be used to attempt a device connection.
+                ///</summary>
+                static
+                    Windows::Foundation::IAsyncOperation<Windows::Devices::Enumeration::DeviceInformationCollection ^> ^
+                    listAvailableDevicesAsync(
+                        void
+                    )
+                {
+                    return BleSerial::listAvailableDevicesAsync();
+                };
 
-private:
-    const uuid_t DFROBOT_BLE_SERVICE_UUID;
-    const uuid_t DFROBOT_BLE_SERIAL_CHARACTERISTIC_UUID;
+            private:
+                BleSerial^ _bleSerial;
+            };
 
-    // Device specific members (set during instantation)
-    Windows::Devices::Enumeration::DeviceInformation ^_device;
-    Platform::String ^_device_name;
-
-    //thread-safe mechanisms. std::unique_lock used to manage the lifecycle of std::mutex
-    std::mutex _mutex;
-    std::unique_lock<std::mutex> _dfrobot_lock;
-
-    std::atomic_bool _connection_ready;
-    Windows::Devices::Enumeration::DeviceInformationCollection ^_device_collection;
-    Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic ^_gatt_characteristic;
-    Windows::Devices::Bluetooth::BluetoothLEDevice ^_gatt_device;
-    Windows::Devices::Bluetooth::GenericAttributeProfile::GattDeviceService ^_gatt_service;
-    std::mutex _q_lock;
-    std::queue<byte> _rx;
-    Windows::Storage::Streams::DataWriter ^_tx;
-
-    Concurrency::task<void>
-    connectToDeviceAsync(
-        Windows::Devices::Enumeration::DeviceInformation ^device_
-        );
-
-    Windows::Devices::Enumeration::DeviceInformation ^
-    identifyDeviceFromCollection(
-        Windows::Devices::Enumeration::DeviceInformationCollection ^devices_
-        );
-
-    void
-    rxCallback(
-        Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic ^sender,
-        Windows::Devices::Bluetooth::GenericAttributeProfile::GattValueChangedEventArgs ^args
-        );
-};
-
-} // namespace Serial
-} // namespace Maker
+        } // namespace Serial
+    } // namespace Maker
 } // namespace Microsoft
